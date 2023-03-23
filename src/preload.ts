feeda70a302878290a2eb347488d5fee87681603
shurintou <shurintou@gmail.com>
2023-03-23 23:44:15 +0900

    Initial commmit

---
+++ src/preload.ts
@@ -0,0 +1,19 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector: string, text: string) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type])
+    }
+})
+
+
+contextBridge.exposeInMainWorld('mouseHandler', {
+    moveMouse: () => ipcRenderer.invoke('mouse-handler:moveMouse'),
+})
